2653e7b0978780a01deb8f6400dc2935c64f4dc3
Tochukwu Ukpai <tochukwuukpai@gmail.com>
2023-05-08 16:20:58 +0100

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Theós on 08/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
